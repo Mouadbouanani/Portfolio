aca22e3c0c1b2b675904a2821157d7ebf89b84f7
mouad <mouadbouanani1@gmail.com>
2025-10-08 13:34:40 +0100

    nothing

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // GitHub Pages deployment
+  base: '/Portfolio/', // GitHub Pages deployment
 })
